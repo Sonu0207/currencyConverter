f31209071c88345be7c552f2dbe66d5575271cae
Sonu Tejwani <sonutejwani13@gmail.com>
2025-07-10 21:36:22 -0400

    Commit 4

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 
-const API_URL = "https://v6.exchangerate-api.com/v6";
+const API_URL = 'https://v6.exchangerate-api.com/v6';
 const API_KEY = process.env.API_KEY;
 //! Rate Limiter 15 minutes - 100 requests
 const apiLimiter = rateLimit({
@@ -21,15 +21,14 @@ app.use(apiLimiter);
 
 //! Conversion Routes
 app.post('/api/convert', async (req, res) => {
-
     try {
         const { from, to, amount } = req.body;
-        console.log({ from, to, amount })
-        const URL = `${API_URL}/${API_KEY}/pair/${from}/${to}/${amount}`;
-        console.log(URL)        
-    } catch (error) {
-        
-    }
+        console.log({from, to, amount})
+        const url = `${API_URL}/${API_KEY}/pair/${from}/${to}/${amount}`;
+        console.log(`Calling URL: ${url}`); // ✅ Logs full API URL
+        const response = await axios.get(url);
+        res.json(response.data);     
+    } catch (error) {}
 })
 
 //! Start Server
